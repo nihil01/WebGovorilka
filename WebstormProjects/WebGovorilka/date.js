327828a5b1ff0b6758fde5cef7729bd5bb14ccf4
nihil01 <pux21602@gmail.com>
2024-04-27 18:50:21 +0400

    Third commit (friends menu)

--- WebstormProjects/WebGovorilka/date.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-
-const year = date.getFullYear().toString().slice(2)
-const month = (date.getMonth() + 1).toString().length > 2 ? (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString().padStart(2, '0')
-const day = date.getDate().toString().length > 2 ? date.getDate().toString() : date.getDate().toString().padStart(2, '0')
-const hours = `${date.getHours()}:${date.getMinutes()}`
-
-const timestamp = `${day}.${month}.${year} | ${hours}`
-module.exports = timestamp;
